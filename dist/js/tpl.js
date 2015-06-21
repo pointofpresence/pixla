26e693aebf85ae98989f7bc8a5731a70d4a0d08b
pointofpresence <git@pointofpresence.ru>
2015-06-21 19:50:52 +0600

    v0.0.8 Logo

--- dist/js/tpl.js
+++ dist/js/tpl.js
@@ -1,8 +1,8 @@
 /**
  * Copyright (c) 2015 pointofpresence
  * ReSampled.Pixla (pixla) - Online image filter
- * @version v0.0.7
- * @build Sun, 21 Jun 2015 12:52:00 GMT
+ * @version v0.0.8
+ * @build Sun, 21 Jun 2015 13:43:07 GMT
  * @link https://github.com/pointofpresence/pixla
  * @license MIT
  */
